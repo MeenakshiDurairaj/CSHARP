f56b64171e28d01c0c5108041e30a97b1cbc8476
Meenakshi Durairaj <dmeena.cse@gmail.com>
2025-05-26 17:37:35 -0400

    Classes added for clean and organized structure

---
+++ ConsoleApp1/Diff_bw_Const_ReadOnly.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Diff_bw_Const_ReadOnly
+{
+    public readonly int x_readonly = 10; // run-time constant
+    public const int y_const = 20;// compile-time constant 
+
+    public Diff_bw_Const_ReadOnly()
+	{
+        Console.WriteLine("The difference between Const and Readonly \n");
+
+        Console.WriteLine("=========================================== \n\n");
+        Console.WriteLine("Const is a compile-time constant, Readonly is a run-time constant \n");
+        Console.WriteLine("Const value cannot be changed. Readonly value can be changed in the constructor \n");
+
+        x_readonly = 30;
+    }
+}
